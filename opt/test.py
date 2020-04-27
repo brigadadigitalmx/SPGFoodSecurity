7094a3c602e604f2310dace8f2df0a0121d53d33
Miguel Escalante <escalas5@gmail.com>
2020-04-26 20:10:45 -0500

    add the testing for the endpoint

--- opt/test.py
+++ opt/test.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from clustering_functions import asigna_entregas
+import requests
 
 
 def generate_dataset(n_casas=100, n_centros=2, n_cuadrillas=[15, 15]):
@@ -13,20 +13,18 @@ def generate_dataset(n_casas=100, n_centros=2, n_cuadrillas=[15, 15]):
         'labs_centros': list(range(n_centros)),
         'lon': np.random.rand(n_centros),
         'lat': np.random.rand(n_centros),
-        'n': [15, 15]})
+        'n': n_cuadrillas})
     return casas_df, centros_df
 
 
 if __name__ == '__main__':
-    n_casas = 6000
+    n_casas = 600
     n_centros = 2
-    casas_df, centros_df = generate_dataset(n_casas, n_centros)
-    labs_casas, labs_centros, labs_cuadrilla = asigna_entregas(
-        casas_df, centros_df)
-
-    df_final = pd.DataFrame({
-        'labs_casas': labs_casas,
-        'labs_centros': labs_centros,
-        'labs_flotilla': labs_cuadrilla
-    })
-    print(df_final.head())
+    n_cuadrillas = [15, 15]
+    casas_df, centros_df = generate_dataset(n_casas, n_centros, n_cuadrillas)
+    r = requests.post('http://0.0.0.0:5000/get_best_routes',
+                      json={
+                          'hogares': casas_df.to_dict(orient='records'),
+                          'centros': centros_df.to_dict(orient='records')
+                      })
+    print(pd.DataFrame(r.json()))
